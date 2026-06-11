25d40648a94a04243e16d1efb535c3d4346b6c12
clen cat <37403253+a145789@users.noreply.github.com>
2026-06-11 22:14:37 +0800

    fix: silent install via /VERYSILENT during auto-update (#17)
    * fix: silent install via /VERYSILENT during auto-update
    
    * nit: use to_wide() for installer params

--- src/update.rs
+++ src/update.rs
@@ -683,11 +683,13 @@ fn launch_installer_and_exit(_hwnd: HWND, installer_path: &str) {
         .collect();
 
     let verb_wide: Vec<u16> = "runas\0".encode_utf16().collect();
+    let params_wide = to_wide("/VERYSILENT /SUPPRESSMSGBOXES /NORESTART");
 
     let mut sei = SHELLEXECUTEINFOW {
         cbSize: std::mem::size_of::<SHELLEXECUTEINFOW>() as u32,
         lpVerb: PCWSTR(verb_wide.as_ptr()),
         lpFile: PCWSTR(path_wide.as_ptr()),
+        lpParameters: PCWSTR(params_wide.as_ptr()),
         nShow: SW_SHOWNORMAL.0,
         ..Default::default()
     };
